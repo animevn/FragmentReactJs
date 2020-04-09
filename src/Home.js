af79ff685b824a429af75e39413db377b6aa9d49
animevn <haanhth21@gmail.com>
2020-04-09 08:47:25 +0700

    first part ok

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 import React, {useState} from "react";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 import {content} from "./Constants";
+import {Radio} from "@material-ui/core";
 
 function Home() {
+  const width = {xs:11, sm:8, md:6, lg:4, xl:3};
   const innital = {option1:"Yes", option2:"No", option3:"None"};
   const [radio, setRadio] = useState(innital.option3);
   const [open, setOpen] = useState(false);
@@ -23,45 +31,48 @@ function Home() {
   }
 
   return (
-    <div className="container mt-3">
-      <div className="row col-xl-7 col-lg-8 col-md-10 col-sm-12 col-12 mx-auto my-3 bg-success
-                      py-2 rounded-lg" style={{"display": open ? "": "none"}}>
-        <div className="text-white">Do you like this?</div>
+    <Grid container direction="column" justify="center" alignItems="center">
+      <Grid item {...width}>
+        <Box flexDirection="row" justifyContent="space-evenly" alignItems="center"
+             my={open ? 1 : 0} display={open ? "flex" : "none"}
+             bgcolor="primary.main" borderRadius={3}>
 
-        <div className="custom-control custom-radio custom-control-inline ml-5">
-          <input type="radio" id="radio1" className="custom-control-input" onChange={handleChange}
-                 value={innital.option1} checked={radio === innital.option1}/>
+          <Typography variant="h6">Do you like this?</Typography>
 
-          <label className="custom-control-label text-white" htmlFor="radio1">Yes</label>
-        </div>
+          <RadioGroup>
+            <FormControlLabel control={
+              <Radio value={innital.option1} checked={radio === innital.option1}
+                     onChange={handleChange} />
+            } label="Yes" />
+          </RadioGroup>
 
-        <div className="custom-control custom-radio custom-control-inline">
-          <input type="radio" id="radio2" className="custom-control-input" onChange={handleChange}
-                 value={innital.option2} checked={radio === innital.option2}/>
+          <RadioGroup>
+            <FormControlLabel control={
+              <Radio value={innital.option2} checked={radio === innital.option2}
+                     onChange={handleChange} />
+            } label="No" />
+          </RadioGroup>
+        </Box>
 
-          <label className="custom-control-label text-white" htmlFor="radio2">No</label>
-        </div>
-      </div>
+        <div className="row col-xl-7 col-lg-8 col-md-10 col-sm-12 col-12 mx-auto my-3 px-0">
+          <div className="d-flex flex-column w-25">
+            <img className="img-thumbnail rounded-lg w-75 h-auto mt-2"
+                 src="images/beatles_anthology_box.webp" alt="beatles"/>
+            <button className="btn btn-outline-success mt-4 w-75" onClick={onOpenClick}>
+              {open ? "Close" : "Open"}
+            </button>
+          </div>
 
-      <div className="row col-xl-7 col-lg-8 col-md-10 col-sm-12 col-12 mx-auto my-3 px-0">
-        <div className="d-flex flex-column w-25">
-          <img className="img-thumbnail rounded-lg w-75 h-auto mt-2"
-               src="images/beatles_anthology_box.webp" alt="beatles"/>
-          <button className="btn btn-outline-success mt-4 w-75" onClick={onOpenClick}>
-            {open ? "Close" : "Open"}
-          </button>
-        </div>
-
-        <div className="w-75">
-          <div className="container pr-0">
-            <h2>Cry For A Shadow</h2>
-            <p className={"text-justify " + (context? "text-success" : "text-dark")}
-               onContextMenu={onSelect}>{content}</p>
+          <div className="w-75">
+            <div className="container pr-0">
+              <h2>Cry For A Shadow</h2>
+              <p className={"text-justify " + (context? "text-success" : "text-dark")}
+                 onContextMenu={onSelect}>{content}</p>
+            </div>
           </div>
         </div>
-      </div>
-
-    </div>
+      </Grid>
+    </Grid>
   )
 }
 export default Home;
--- src/indexTheme.js
+++ src/indexTheme.js
@@ -17,14 +17,14 @@ const indexTheme = createMuiTheme({
       default: orange["50"],
     },
   },
-  // typography:{
-  //   h3:{
-  //     fontSize: '2rem',
-  //     '@media (min-width:600px)': {fontSize: '2.5rem'},
-  //     '@media (min-width:960px)': {fontSize: '3rem'},
-  //     '@media (min-width:1280px)': {fontSize: '3.5rem'},
-  //   }
-  // }
+  typography:{
+    h3:{
+      fontSize: '2rem',
+      '@media (min-width:600px)': {fontSize: '2.5rem'},
+      '@media (min-width:960px)': {fontSize: '3rem'},
+      '@media (min-width:1280px)': {fontSize: '3.5rem'},
+    }
+  }
 });
 
 export default indexTheme;
